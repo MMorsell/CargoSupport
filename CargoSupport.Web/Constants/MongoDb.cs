f5c17cb41a556a8d380059d1869ecd69fb70197d
Martin Morsell <martinmorsell18@gmail.com>
2020-08-18 20:34:20 +0200

    added build configurations

--- CargoSupport.Web/Constants/MongoDb.cs
+++ CargoSupport.Web/Constants/MongoDb.cs
@@ -7,7 +7,11 @@ namespace CargoSupport.Constants
 {
     public class MongoDb
     {
+#if DEBUG
         public static string ConnectionString => "mongodb://root:example@host.docker.internal:27017";
+#else
+        public static string ConnectionString => "mongodb://root:example@192.168.1.81:27017";
+#endif
         public static string DatabaseName => "ICDB";
         public static string OutputScreenTableName => "ICOutputScreen";
         public static string BackupCollectionName => "ICOutputScreen_Backup";
