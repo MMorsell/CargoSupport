0a1d6f8df8ab9e9d4ca5c3bb9ba003eae8f35fa1
Martin Morsell <martinmorsell18@gmail.com>
2020-10-03 19:28:17 +0200

    completed update animation on row that is updated, Closes #4

--- CargoSupport.Web.IIS/wwwroot/js/ChartFunctions.js
+++ CargoSupport.Web.IIS/wwwroot/js/ChartFunctions.js
@@ -93,112 +93,143 @@ function updateSingleRecord(newObject) {
     if (result[0] !== -1) {
         const object = result[0];
         const index = result[1];
+        let update = false;
 
         if (window.location.href.includes('Transport')) {
             //Transport view only cares about updates with these properties
             if (newObject["preRideInput"] !== undefined) {
                 object.preRideAnnotation = newObject["preRideInput"];
+                update = true;
             }
             else if (newObject["postRideInput"] !== undefined) {
                 object.postRideAnnotation = newObject["postRideInput"];
+                update = true;
             }
             else if (newObject["port_selectBox"] !== undefined) {
                 object.portNumber = parseInt(newObject["port_selectBox"], 10);
+                update = true;
             }
             else if (newObject["carNumber_selectBox"] !== undefined) {
                 object.carNumber = newObject["carNumber_selectBox"];
+                update = true;
             }
             else if (newObject["convert_loadingLevel_toSelectbox"] !== undefined) {
                 object.loadingLevel = newObject["convert_loadingLevel_toSelectbox"];
+                update = true;
             }
             else if (newObject["driver_select"] !== undefined) {
                 object.driver.id = newObject["driver_select"];
+                update = true;
             }
             else if (newObject["numberOfColdBoxes_input"] !== undefined) {
                 object.numberOfColdBoxes = newObject["numberOfColdBoxes_input"];
+                update = true;
             }
             else if (newObject["restPicking_input"] !== undefined) {
                 object.restPlock = newObject["restPicking_input"];
+                update = true;
             }
             else if (newObject["numberOfFrozenBoxes_input"] !== undefined) {
                 object.numberOfFrozenBoxes = newObject["numberOfFrozenBoxes_input"];
+                update = true;
             }
             else if (newObject["numberOfBreadBoxes_input"] !== undefined) {
                 object.numberOfBreadBoxes = newObject["numberOfBreadBoxes_input"];
+                update = true;
             }
             else if (newObject["controlIsDone_input"] !== undefined) {
                 object.controlIsDone = newObject["controlIsDone_input"];
+                update = true;
             }
         } else if (window.location.href.includes('Plock')) {
             //Plock view only cares about updates with these properties
             if (newObject["numberOfColdBoxes_input"] !== undefined) {
                 object.numberOfColdBoxes.value = newObject["numberOfColdBoxes_input"];
+                update = true;
             }
             else if (newObject["carNumber_selectBox"] !== undefined) {
                 object.carNumber = newObject["carNumber_selectBox"];
+                update = true;
             }
             else if (newObject["port_selectBox"] !== undefined) {
                 object.portNumber = newObject["port_selectBox"];
+                update = true;
             }
             else if (newObject["restPicking_input"] !== undefined) {
                 object.restPicking.value = newObject["restPicking_input"];
+                update = true;
             }
             else if (newObject["numberOfFrozenBoxes_input"] !== undefined) {
                 object.numberOfFrozenBoxes.value = newObject["numberOfFrozenBoxes_input"];
+                update = true;
             }
             else if (newObject["numberOfBreadBoxes_input"] !== undefined) {
                 object.numberOfBreadBoxes.value = newObject["numberOfBreadBoxes_input"];
+                update = true;
             }
             else if (newObject["controlIsDone_input"] !== undefined) {
                 object.controlIsDone.value = newObject["controlIsDone_input"];
+                update = true;
             }
             else if (newObject["convert_loadingLevel_toSelectbox"] !== undefined) {
                 object.loadingLevel = newObject["convert_loadingLevel_toSelectbox"];
+                update = true;
             }
         }
         else {
             //Medarbetare view only cares about updates with these properties
             if (newObject["preRideInput"] !== undefined) {
                 object.preRideAnnotation = newObject["preRideInput"];
+                update = true;
             }
             else if (newObject["port_selectBox"] !== undefined) {
                 object.portNumber = newObject["port_selectBox"];
+                update = true;
             }
             else if (newObject["carNumber_selectBox"] !== undefined) {
                 object.carNumber = newObject["carNumber_selectBox"];
+                update = true;
             }
             else if (newObject["convert_loadingLevel_toSelectbox"] !== undefined) {
                 object.loadingLevel = newObject["convert_loadingLevel_toSelectbox"];
+                update = true;
             }
             else if (newObject["driver_select"] !== undefined) {
                 object.driver.fullName = newObject["driver_fullName"];
+                update = true;
             }
             else if (newObject["numberOfBreadBoxes_input"] !== undefined) {
                 object.numberOfBreadBoxes = newObject["numberOfBreadBoxes_input"];
+                update = true;
             }
             else if (newObject["restPicking_input"] !== undefined) {
                 object.restPlock = newObject["restPicking_input"];
+                update = true;
             }
             else if (newObject["numberOfFrozenBoxes_input"] !== undefined) {
                 object.numberOfFrozenBoxes = newObject["numberOfFrozenBoxes_input"];
+                update = true;
             }
             else if (newObject["numberOfBreadBoxes"] !== undefined) {
                 object.numberOfBreadBoxes = newObject["numberOfBreadBoxes"];
+                update = true;
             }
             else if (newObject["numberOfColdBoxes_input"] !== undefined) {
                 object.numberOfColdBoxes = newObject["numberOfColdBoxes_input"];
+                update = true;
             }
             else if (newObject["controlIsDone_input"] !== undefined) {
                 object.controlIsDone = newObject["controlIsDone_input"];
+                update = true;
             }
         }
 
-        table.row(index).invalidate();
-        //table.row(index).remove().draw()
-        //table.row.add(object).draw()
-        //$('#dataTable').dataTable().fnUpdate(object, index, undefined, false);
-        //$("tr:eq( " + (index + 1) + " )").removeClass("updateanimation");
-        //setTimeout(function () { $("tr:eq( " + (index + 1) + " )").addClass("updateanimation"); }, 100);
-        console.log('updated single record')
+        if (update) {
+            table.row(index).invalidate();
+            var row = table.row(index).node();
+            $(row).removeClass('updateanimation');
+            setTimeout(function () { $(row).addClass('updateanimation'); }, 50);
+            console.log('updated single record')
+        }
     }
 }
