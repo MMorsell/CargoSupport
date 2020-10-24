c77f019eab55076a19e49720f546c89f70e6bb64
Martin Morsell <martinmorsell18@gmail.com>
2020-10-24 20:12:33 +0200

    added temp. updates for new definitions in database

--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -8,48 +8,8 @@ namespace TestingConsole
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Update started");
-            var dbservice = new MongoDbService("mongodb://localhost:27017", "ICDB");
-            Console.WriteLine("Loading allrecords");
-            var allRecords = dbservice.GetAllRecords<DataModel>(CargoSupport.Constants.MongoDb.OutputScreenCollectionName).Result;
-            Console.WriteLine($"All records loaded, {allRecords.Count}");
-            foreach (var record in allRecords)
-            {
-                foreach (var customer in record.PinRouteModel.Customers)
-                {
-                    var resArr = customer.tracking_number.Split('-');
-                    if (resArr.Length > 1)
-                    {
-                        customer.tracking_number_splitted = resArr[1];
-                    }
-                    else
-                    {
-                        customer.tracking_number_splitted = "NO_tracking_number_splitted";
-                    }
-                }
-            }
-            Console.WriteLine($"Updated all records in memory, starting update");
-            int numberOfErrors = 0;
-            foreach (var record in allRecords)
-            {
-                try
-                {
-                    dbservice.UpsertDataRecord(CargoSupport.Constants.MongoDb.OutputScreenCollectionName, record).Wait();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    Console.WriteLine("ERROR");
-                    numberOfErrors++;
-                }
-            }
-            Console.WriteLine($"Done with update, {numberOfErrors} errors");
-            Console.Read();
-            Console.Read();
-            Console.Read();
-            Console.Read();
-            Console.Read();
-            Console.Read();
+            UpgradeCars();
+            //UpgradeCustomers();
 
             //var _dbHelper = new MongoDbHelper(CargoSupport.Constants.MongoDb.DatabaseName);
 
@@ -106,6 +66,87 @@ namespace TestingConsole
             //var r = _dbhelper.GetRecordById<PinRouteModel>(CargoSupport.Constants.MongoDb.OutputScreenTableName, new Guid("e46b2b7f-0d96-4716-9a73-f58242660561")).Result;
             //Console.WriteLine(r.RouteName);
             //Console.WriteLine("Hello World!");
+        }
+
+        private static void UpgradeCars()
+        {
+            Console.WriteLine("Update started");
+            var dbservice = new MongoDbService("mongodb://localhost:27017", "ICDB");
+            Console.WriteLine("Loading allrecords");
+            var allRecords = dbservice.GetAllRecords<CarModel>(CargoSupport.Constants.MongoDb.CarCollectionName).Result;
+            Console.WriteLine($"All records loaded, {allRecords.Count}");
+            foreach (var car in allRecords)
+            {
+                car.IsRetired = false;
+            }
+            Console.WriteLine($"Updated all records in memory, starting update");
+            int numberOfErrors = 0;
+            foreach (var record in allRecords)
+            {
+                try
+                {
+                    dbservice.UpsertCarRecordById(CargoSupport.Constants.MongoDb.CarCollectionName, record).Wait();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("ERROR");
+                    numberOfErrors++;
+                }
+            }
+            Console.WriteLine($"Done with update, {numberOfErrors} errors");
+            Console.Read();
+            Console.Read();
+            Console.Read();
+            Console.Read();
+            Console.Read();
+            Console.Read();
+        }
+
+        private static void UpgradeCustomers()
+        {
+            Console.WriteLine("Update started");
+            var dbservice = new MongoDbService("mongodb://localhost:27017", "ICDB");
+            Console.WriteLine("Loading allrecords");
+            var allRecords = dbservice.GetAllRecords<DataModel>(CargoSupport.Constants.MongoDb.OutputScreenCollectionName).Result;
+            Console.WriteLine($"All records loaded, {allRecords.Count}");
+            foreach (var record in allRecords)
+            {
+                foreach (var customer in record.PinRouteModel.Customers)
+                {
+                    var resArr = customer.tracking_number.Split('-');
+                    if (resArr.Length > 1)
+                    {
+                        customer.tracking_number_splitted = resArr[1];
+                    }
+                    else
+                    {
+                        customer.tracking_number_splitted = "NO_tracking_number_splitted";
+                    }
+                }
+            }
+            Console.WriteLine($"Updated all records in memory, starting update");
+            int numberOfErrors = 0;
+            foreach (var record in allRecords)
+            {
+                try
+                {
+                    dbservice.UpsertDataRecord(CargoSupport.Constants.MongoDb.OutputScreenCollectionName, record).Wait();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("ERROR");
+                    numberOfErrors++;
+                }
+            }
+            Console.WriteLine($"Done with update, {numberOfErrors} errors");
+            Console.Read();
+            Console.Read();
+            Console.Read();
+            Console.Read();
+            Console.Read();
+            Console.Read();
         }
     }
 }
