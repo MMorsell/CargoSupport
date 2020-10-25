7e9fa9f1d7ea81f288ce8790abeabbf5b94fc74f
Martin Morsell <martinmorsell18@gmail.com>
2020-10-25 10:15:33 +0100

    fixed weird auto-formatting from vs that caused the script to crash

--- CargoSupport.Web.IIS/BackupDatabase.ps1
+++ CargoSupport.Web.IIS/BackupDatabase.ps1
@@ -1,3 +1,3 @@
-﻿$date = Get - Date - Format "yyyy-MM-dd"
-New - Item - ItemType Directory - Path "D:\DBBackups\$date"
+﻿$date = Get-Date -Format "yyyy-MM-dd"
+New-Item -ItemType Directory -Path "D:\DBBackups\$date"
 & "D:\DBBackups\Tools\bin\mongodump.exe"--out D:\DBBackups\$date\ --db ICDB
